fc2db05b701424a4fb88d33c3a1945cf79510d05
kishimoto <kishimoto@mi-rai.co.jp>
2021-02-15 18:48:46 +0900

    アップデートチェック

--- hp-reservia.php
+++ hp-reservia.php
@@ -4,7 +4,7 @@ Plugin Name: Reservia ReViews for HairsPress
 Plugin URI: https://github.com/nullpon16tera/hp-reservia
 Description: Reservia API on HairsPress. Reservia reviews page.
 Author: MIRAI
-Version: 1.1.10
+Version: 1.1.11
 Author URI: https://mi-rai.co.jp/
 */
 require __DIR__.'/plugin-update-checker/plugin-update-checker.php';
@@ -17,8 +17,8 @@ $hpReserviaChecker = Puc_V4_Factory::buildUpdateChecker(
 	'hp-reservia'
 );
 
-$hpReserviaChecker->setAuthentication('1b4a59d8e9f55ecc2d2999d564051101883bbea7');
-$hpReserviaChecker->setBranch('master');
+$hpReserviaChecker->setAuthentication('1407214a37299d2a0947f9c37dc8abce56a563c4');
+// $hpReserviaChecker->setBranch('master');
 $hpReserviaChecker->getVcsApi()->enableReleaseAssets();
 
 class HP_Reservia
